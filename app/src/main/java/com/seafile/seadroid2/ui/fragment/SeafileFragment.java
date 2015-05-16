0538c980a93a08f8d74ebf2e20c6dc5ff0587a52
hangox <liang.hanguang93@gmail.com>
2015-05-16 20:54:55 +0800

    add 提交完整代码

---
+++ app/src/main/java/com/seafile/seadroid2/ui/fragment/SeafileFragment.java
@@ -0,0 +1,12 @@
+package com.seafile.seadroid2.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created With Android Studio
+ * User 47
+ * Date 2015/5/16
+ * Time 18:15
+ */
+public class SeafileFragment extends Fragment {
+}
